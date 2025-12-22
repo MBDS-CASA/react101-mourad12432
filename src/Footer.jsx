2a3d8088082ce9383d999a83651ad661a9f5fa37
gohstblack <v1236341@gmail.com>
2025-12-22 15:51:10 +0100

    TD01 : Ajouter un footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer() {
   return (
     <footer className="footer">
-      <p>Tous droits réservés - [Nom] [Prénom]</p>
+      <p>Tous droits réservés - Bounaim Mourad</p>
     </footer>
   )
 }
