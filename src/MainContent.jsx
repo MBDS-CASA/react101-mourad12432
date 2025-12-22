05e9d7a7152878641bc8af6d48becf8106724fdf
gohstblack <v1236341@gmail.com>
2025-12-22 15:42:29 +0100

    TD01 : Ajouter un composant pour le contenu principal de la page

---
+++ src/MainContent.jsx
@@ -0,0 +1,11 @@
+import './MainContent.css'
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <p>Ici, nous afficherons des informations interessantes :)</p>
+    </div>
+  )
+}
+
+export default MainContent
